6cd8ea45a233fe7037671efbdb0f833082cb3fbf
Ariane Ancrenaz <ancrenaz.ariane@hotmail.fr>
2018-05-16 14:37:43 +0200

    write_compressed

--- header/utilitaire_compression.h
+++ header/utilitaire_compression.h
@@ -1,27 +1,29 @@
 #ifndef UTIL_C
-#define	UTIL_C
+#define UTIL_C
 #include "huffman.h"
 
 void parcours_arbre(canonical_tree* tree, char* tableau, int profondeur);
 char* tree_to_length_table(canonical_tree* tree);
 int lire_symbole(FILE* f);
-void ecrire_symbole(FILE* f,char c);
+void ecrire_symbole(FILE* f, char c);
 char encoder_symbole(canonical_tree* tree, char symbole, int* lg);
-void afficher_arbre (noeud* tete, int niveau);
+void afficher_arbre(noeud* tete, int niveau);
 
-void tricroissant( int** tab, int tab_size);
-noeud** tableau_noeud( int **tabCara,int nbCara);
-void tricroissantNoeud( noeud** tab, int tab_size);
+void tricroissant(int** tab, int tab_size);
+noeud** tableau_noeud(int** tabCara, int nbCara);
+void tricroissantNoeud(noeud** tab, int tab_size);
 
+int traitement_caractere(int* cmp, int lg, char* octet, char* buffer,
+                         FILE* dst);
 
 /**
-* encoder_symbole : renvoit le code d'un symbole (char ascii) dans un arbre quelconque
-* On renvoit un octet (char).
-* Le code du symbole est placé dans les bits de poids faible.
-* Le pointeur sur entier lg contient la longueur (en nombre de bit) du symbole codé. 
-* Attention : Le resultat peut donc etre plus court qu'un octet.
-* Convention : le poid d'un fils_gauche est mis à 0 et celui d'un fils_droit à 1. 
-**/
+ * encoder_symbole : renvoit le code d'un symbole (char ascii) dans un arbre
+ *quelconque On renvoit un octet (char). Le code du symbole est placé dans les
+ *bits de poids faible. Le pointeur sur entier lg contient la longueur (en
+ *nombre de bit) du symbole codé. Attention : Le resultat peut donc etre plus
+ *court qu'un octet. Convention : le poid d'un fils_gauche est mis à 0 et celui
+ *d'un fils_droit à 1.
+ **/
 char encoder_symbole(huffman_tree* tree, char symbole, int* lg);
 
 /**
@@ -32,17 +34,16 @@ renvoit NULL si le symbole n'est pas trouvé
 **/
 noeud* recherche_symbole_arbre(tree* tree, char symbole);
 
-
 /* Ergi */
 
-int nb_feuilles (huffman_tree* tree);
+int nb_feuilles(huffman_tree* tree);
 
-int tree_depth (huffman_tree* a);
+int tree_depth(huffman_tree* a);
 
-void construction_par_niveau(huffman_tree* tree, int level, int longueur, 
-                                int* p_indice, tableau_constructif* tab) ;
+void construction_par_niveau(huffman_tree* tree, int level, int longueur,
+                             int* p_indice, tableau_constructif* tab);
 
-void tri_tableau(tableau_constructif *tab, int nbf);
+void tri_tableau(tableau_constructif* tab, int nbf);
 /* Fin Ergi */
 
 #endif
--- src/utilitaire_compression.c
+++ src/utilitaire_compression.c
@@ -1,215 +1,202 @@
+#include "utilitaire_compression.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "huffman.h"
 #include "functions.h"
-#include "utilitaire_compression.h"
-
+#include "huffman.h"
 
 /* JULIETTE */
 /**
-* encoder_symbole : renvoit le code d'un symbole (char ascii) dans un arbre quelconque
-* On renvoit une chaine de caractère.
-* Le code du symbole est placé dans l'ordre
-* Le pointeur sur entier lg contient la longueur (en nombre de bit) du symbole codé. 
-* Attention : Le resultat peut donc etre plus court qu'un octet.
-* Convention : le poid d'un fils_gauche est mis à 0 et celui d'un fils_droit à 1. 
-**/
-char* encoder_symbole(tree* tree, char symbole, int* lg){
-	noeud *n;
-	char res[32];
+ * encoder_symbole : renvoit le code d'un symbole (char ascii) dans un arbre
+ *quelconque On renvoit une chaine de caractère. Le code du symbole est placé
+ *dans l'ordre Le pointeur sur entier lg contient la longueur (en nombre de bit)
+ *du symbole codé. Attention : Le resultat peut donc etre plus court qu'un
+ *octet. Convention : le poid d'un fils_gauche est mis à 0 et celui d'un
+ *fils_droit à 1.
+ **/
+char* encoder_symbole(tree* tree, char symbole, int* lg) {
+  noeud* n;
+  char res[32];
 
-	int i=0;
-	int j=0;
+  int i = 0;
+  int j = 0;
 
-	int *profondeur = 0;
-	
-	//on initialise le tableau
-	for(int k=0; k<32; k++){
-		res[k]=0;
-	}
+  int* profondeur = 0;
 
-	//parcours de l'arbre en recherchant sym
-	n=recherche_symbole_arbre(tree,symbole, profondeur);
+  // on initialise le tableau
+  for (int k = 0; k < 32; k++) {
+    res[k] = 0;
+  }
 
-	//le nombre de bit necessaire pour ecrire le symbole codé
-	(*lg)=(*profondeur);
+  // parcours de l'arbre en recherchant sym
+  n = recherche_symbole_arbre(tree, symbole, profondeur);
 
-	//i indique dans quelle case du tableau on doit commencer à écrire
-	i=(*profondeur)/8 -1;
-	if(*profondeur %8 != 0) i++;
-	//on indique a partir de quel rang on doit ecrire les bits
-	j=(7-((*profondeur)%8)-1) %8;
+  // le nombre de bit necessaire pour ecrire le symbole codé
+  (*lg) = (*profondeur);
 
-	//ecriture du code de sym
-	//tant qu'on ne pointe pas sur la tete, c'est à dire qu'on a fini de remonter l'arbre
-	while ( n != tree ){
-		assert(i>=0 && j>=0 && j<8);
-		//si la branche vaut 1 (fils droit)
-		if (n == n->parent->fils_droit){
-			res[i] +=  1 << j; //on ecrit 1 (bit) décallé en fonction de la profondeur
-		}
+  // i indique dans quelle case du tableau on doit commencer à écrire
+  i = (*profondeur) / 8 - 1;
+  if (*profondeur % 8 != 0) i++;
+  // on indique a partir de quel rang on doit ecrire les bits
+  j = (7 - ((*profondeur) % 8) - 1) % 8;
 
-		//si la branche vaut 0  (fils gauche) decalage inutile
+  // ecriture du code de sym
+  // tant qu'on ne pointe pas sur la tete, c'est à dire qu'on a fini de remonter
+  // l'arbre
+  while (n != tree) {
+    assert(i >= 0 && j >= 0 && j < 8);
+    // si la branche vaut 1 (fils droit)
+    if (n == n->parent->fils_droit) {
+      res[i] += 1 << j;  // on ecrit 1 (bit) décallé en fonction de la
+                         // profondeur
+    }
 
-		//on incremente
-		n = n->parent;
-		//longueur du code du symbole restant à ecrire
-		j++;
+    // si la branche vaut 0  (fils gauche) decalage inutile
 
-		//si on commence un nouvel octet
-		if(j == 8){
-			i--;
-			j=0;
-		}
-	}
+    // on incremente
+    n = n->parent;
+    // longueur du code du symbole restant à ecrire
+    j++;
 
+    // si on commence un nouvel octet
+    if (j == 8) {
+      i--;
+      j = 0;
+    }
+  }
 
-
-	return (char*)res;
-
+  return (char*)res;
 }
 
 /**
-* Parcours d'un arbre en recherchant un symbole (char ascii)
-* fonction recursive
-* renvoit NULL si le symbole n'est pas trouvé
-* renvoit le noeud correspondant au symbole
-**/
-noeud* recherche_symbole_arbre(tree* tree, char symbole, int* profondeur){
-	noeud* n;
+ * Parcours d'un arbre en recherchant un symbole (char ascii)
+ * fonction recursive
+ * renvoit NULL si le symbole n'est pas trouvé
+ * renvoit le noeud correspondant au symbole
+ **/
+noeud* recherche_symbole_arbre(tree* tree, char symbole, int* profondeur) {
+  noeud* n;
 
-	//si on arrive en fin d'arbre
-	if(tree == NULL){
-		return NULL;
-	}
+  // si on arrive en fin d'arbre
+  if (tree == NULL) {
+    return NULL;
+  }
 
-	//si on a trouvé le charactère
-	if(tree->caractere == symbole){
-		return tree;
-	}
-	//sinon on parcours le fils droit pour le trouver
-	else if(n = recherche_symbole_arbre(tree->fils_droit, symbole) != NULL){
-		(*profondeur)++;
-		return n;
-	}
-	//et si il n'est pas dans le fils droit on parcours le fils gauche
-	else {
-		(*profondeur)++;
-		return recherche_symbole_arbre(tree->fils_gauche, symbole);
-	}
-
+  // si on a trouvé le charactère
+  if (tree->caractere == symbole) {
+    return tree;
+  }
+  // sinon on parcours le fils droit pour le trouver
+  else if (n = recherche_symbole_arbre(tree->fils_droit, symbole) != NULL) {
+    (*profondeur)++;
+    return n;
+  }
+  // et si il n'est pas dans le fils droit on parcours le fils gauche
+  else {
+    (*profondeur)++;
+    return recherche_symbole_arbre(tree->fils_gauche, symbole);
+  }
 }
-
 
 /* ARIANE */
 
-
 void parcours_arbre(canonical_tree* tree, char* tableau, int profondeur) {
-	if (tree == NULL) {
-		return;
-	} else {
-		if (tree->caractere != -1) {
-			tableau[tree->caractere] = profondeur;
-		}
-		parcours_arbre(tree->fils_gauche, tableau, profondeur + 1);
-		parcours_arbre(tree->fils_droite, tableau, profondeur + 1);
-	}
+  if (tree == NULL) {
+    return;
+  } else {
+    if (tree->caractere != -1) {
+      tableau[tree->caractere] = profondeur;
+    }
+    parcours_arbre(tree->fils_gauche, tableau, profondeur + 1);
+    parcours_arbre(tree->fils_droite, tableau, profondeur + 1);
+  }
 }
 
 char* tree_to_length_table(canonical_tree* tree) {
-	char* table = malloc(sizeof(char) * 256);
-	for (int i = 0; i < 256; i++) {
-		table[i] = 0;
-	}
-	return table;
+  char* table = malloc(sizeof(char) * 256);
+  for (int i = 0; i < 256; i++) {
+    table[i] = 0;
+  }
+  return table;
 }
 
 /* QUENTIN */
 
+void tricroissant(int** tab, int tab_size) {
+  int i = 0;
+  int tmp0 = 0;
+  int tmp1 = 0;
+  int j = 0;
 
-void tricroissant( int** tab, int tab_size){
-  int i=0;
-  int tmp0=0;
-  int tmp1=0;
-  int j=0;
+  for (i = 0; i < tab_size; i++)  // On veut remplir la case i du tableau
+  {
+    for (j = i + 1; j < tab_size;
+         j++)  // On vérifie s'il n'y a pas de nombre inférieur
+    {          // Dans les cases suivantes
+      if (tab[j][1] < tab[i][1]) {
+        tmp0 = tab[i][0];  // Si c'est le cas on intervertit les cases
+        tab[i][0] = tab[j][0];
+        tab[j][0] = tmp0;
 
-  for(i = 0; i < tab_size; i++)          //On veut remplir la case i du tableau
-    {
-      for(j = i+1; j < tab_size; j++)    //On vérifie s'il n'y a pas de nombre inférieur
-        {                                //Dans les cases suivantes
-          if(tab[j][1] < tab[i][1])
-            {
-
-              tmp0 = tab[i][0];              //Si c'est le cas on intervertit les cases
-              tab[i][0] = tab[j][0];
-              tab[j][0] = tmp0;
-
-              tmp1 = tab[i][1];              //Si c'est le cas on intervertit les cases
-              tab[i][1] = tab[j][1];
-              tab[j][1] = tmp1;
-            }
-        }
+        tmp1 = tab[i][1];  // Si c'est le cas on intervertit les cases
+        tab[i][1] = tab[j][1];
+        tab[j][1] = tmp1;
+      }
     }
+  }
 }
 
-
-noeud** tableau_noeud( int **tabCara,int nbCara){
-	int i ;
+noeud** tableau_noeud(int** tabCara, int nbCara) {
+  int i;
   noeud** tabNoeud = malloc(nbCara * sizeof(noeud*));
 
-	for ( i =0; i<nbCara ; i++) {
-    noeud* temp = malloc ( sizeof (noeud) ) ;
-		temp->poid = tabCara[i][1] ;
+  for (i = 0; i < nbCara; i++) {
+    noeud* temp = malloc(sizeof(noeud));
+    temp->poid = tabCara[i][1];
     temp->caractere = tabCara[i][0];
-		temp->fils_droite = NULL;
-		temp->fils_gauche = NULL;
+    temp->fils_droite = NULL;
+    temp->fils_gauche = NULL;
     temp->pere = NULL;
-	  tabNoeud[i] = temp ;
-	}
+    tabNoeud[i] = temp;
+  }
 
   return tabNoeud;
-
 }
 
-
-void tricroissantNoeud( noeud** tab, int tab_size)
-{
-  int i=0;
+void tricroissantNoeud(noeud** tab, int tab_size) {
+  int i = 0;
   noeud* tmp;
-  int j=0;
+  int j = 0;
 
-  for(i = 0; i < tab_size; i++)          //On veut remplir la case i du tableau
-    {
-      for(j = i+1; j < tab_size; j++)    //On vérifie s'il n'y a pas de nombre inférieur
-        {                                //Dans les cases suivantes
-          if(tab[j]->poid < tab[i]->poid)
-            {
-              tmp = tab[i];              //Si c'est le cas on intervertit les cases
-              tab[i] = tab[j];
-              tab[j] = tmp;
-
-            }
-        }
+  for (i = 0; i < tab_size; i++)  // On veut remplir la case i du tableau
+  {
+    for (j = i + 1; j < tab_size;
+         j++)  // On vérifie s'il n'y a pas de nombre inférieur
+    {          // Dans les cases suivantes
+      if (tab[j]->poid < tab[i]->poid) {
+        tmp = tab[i];  // Si c'est le cas on intervertit les cases
+        tab[i] = tab[j];
+        tab[j] = tmp;
+      }
     }
+  }
 }
 
-void afficher_arbre (noeud* tete, int niveau) {
-	int i ;
-	if (tete != NULL) {
-		afficher_arbre (tete->fils_droite, niveau+1);
-		for ( i = 0; i < niveau ; i++){
-			printf ("\t") ;
-		}
-		if(tete->caractere != -1){
-			printf (" %d (%c)\n\n" , tete->poid, tete->caractere ) ;
-		}
-		else{
-			printf (" %d (NULL)\n\n" ,tete->poid) ;
-		}
+void afficher_arbre(noeud* tete, int niveau) {
+  int i;
+  if (tete != NULL) {
+    afficher_arbre(tete->fils_droite, niveau + 1);
+    for (i = 0; i < niveau; i++) {
+      printf("\t");
+    }
+    if (tete->caractere != -1) {
+      printf(" %d (%c)\n\n", tete->poid, tete->caractere);
+    } else {
+      printf(" %d (NULL)\n\n", tete->poid);
+    }
 
-		afficher_arbre (tete->fils_gauche , niveau+1);
-	}
-	return ;
+    afficher_arbre(tete->fils_gauche, niveau + 1);
+  }
+  return;
 }
 
 /* RAPHAEL */
@@ -218,59 +205,71 @@ void afficher_arbre (noeud* tete, int niveau) {
 
 /* Ergi */
 
+void construction_par_niveau(huffman_tree* tree, int level, int longueur,
+                             int* p_indice, tableau_constructif* tab) {
+  if (tree == NULL) return;
 
-void construction_par_niveau(huffman_tree *tree, int level, int longueur,
-                             int *p_indice, tableau_constructif *tab)
-{
-    if (tree == NULL)
-        return;
-
-    if (level == 0)
-    {
-        if (tree->fils_gauche == NULL && tree->fils_droite == NULL)
-        {
-            tab[*p_indice].caractere = tree->caractere;
-            tab[*p_indice].longueur = longueur;
-            (*p_indice)++;
-        }
+  if (level == 0) {
+    if (tree->fils_gauche == NULL && tree->fils_droite == NULL) {
+      tab[*p_indice].caractere = tree->caractere;
+      tab[*p_indice].longueur = longueur;
+      (*p_indice)++;
     }
+  }
 
-    else if (level > 0)
-    {
-        construction_par_niveau(tree->fils_gauche, level - 1, longueur, p_indice, tab);
-        construction_par_niveau(tree->fils_droite, level - 1, longueur, p_indice, tab);
-    }
+  else if (level > 0) {
+    construction_par_niveau(tree->fils_gauche, level - 1, longueur, p_indice,
+                            tab);
+    construction_par_niveau(tree->fils_droite, level - 1, longueur, p_indice,
+                            tab);
+  }
 }
 
-void tri_tableau(tableau_constructif *tab, int nbf)
-{
-    int indice_car_min;
-    int temp;
-    int inf = 0;
-    int sup = 0;
-    int longueur;
+void tri_tableau(tableau_constructif* tab, int nbf) {
+  int indice_car_min;
+  int temp;
+  int inf = 0;
+  int sup = 0;
+  int longueur;
 
-    while (sup < nbf)
-    {
-        longueur = tab[inf].longueur;
-        while (longueur == tab[sup].longueur)
-            sup++;
+  while (sup < nbf) {
+    longueur = tab[inf].longueur;
+    while (longueur == tab[sup].longueur) sup++;
 
-        while (inf < sup)
-        {
-            indice_car_min = inf;
-            int i = inf;
-            while (i < sup)
-            {
-                if (tab[i].caractere < tab[indice_car_min].caractere)
-                    indice_car_min = i;
-                i++;
-            }
-            temp = tab[inf].caractere;
-            tab[inf].caractere = tab[indice_car_min].caractere;
-            tab[indice_car_min].caractere = temp;
-            inf++;
-        }
+    while (inf < sup) {
+      indice_car_min = inf;
+      int i = inf;
+      while (i < sup) {
+        if (tab[i].caractere < tab[indice_car_min].caractere)
+          indice_car_min = i;
+        i++;
+      }
+      temp = tab[inf].caractere;
+      tab[inf].caractere = tab[indice_car_min].caractere;
+      tab[indice_car_min].caractere = temp;
+      inf++;
     }
+  }
 }
 
+int traitement_caractere(int* cmp, int lg, char* octet, char* buffer,
+                         FILE* dst) {
+  if (*cmp + lg < 8) {
+    *octet = (*octet << lg) | (*buffer);
+    *cmp = *cmp + lg;
+    return lg;
+  } else if (*cmp + lg == 8) {
+    *octet = (*octet << lg) | (*buffer);
+    ecrire_symbole(dst, *octet);
+    *octet = 0;
+    *cmp = 0;
+    return lg;
+  } else {
+    char temp = *buffer;
+    *octet = (*octet << (8 - *cmp)) | (temp >> (8 - *cmp));
+    ecrire_symbole(dst, *octet);
+    *octet = (*buffer << *cmp) >> *cmp;
+    *cmp = lg - (8 - *cmp);
+    return *cmp;
+  }
+}
