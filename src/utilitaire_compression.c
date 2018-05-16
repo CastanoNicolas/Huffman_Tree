44cd9de3ae73ce1842d73f3e942ff1b5f43d0aa8
Juliette LAPLANTE <juliette.laplante.jl@gmail.com>
2018-05-16 12:10:47 +0200

    fonctione encoder octet non testé

--- src/utilitaire_compression.c
+++ src/utilitaire_compression.c
@@ -19,8 +19,8 @@ int lire_symbole(FILE* f){
 
 /**
 * encoder_symbole : renvoit le code d'un symbole (char ascii) dans un arbre quelconque
-* On renvoit un octet (char).
-* Le code du symbole est placé dans les bits de poids faible.
+* On renvoit une chaine de caractère.
+* Le code du symbole est placé dans l'ordre
 * Le pointeur sur entier lg contient la longueur (en nombre de bit) du symbole codé. 
 * Attention : Le resultat peut donc etre plus court qu'un octet.
 * Convention : le poid d'un fils_gauche est mis à 0 et celui d'un fils_droit à 1. 
@@ -28,37 +28,52 @@ int lire_symbole(FILE* f){
 char* encoder_symbole(tree* tree, char symbole, int* lg){
 	noeud *n;
 	char res[32];
+
 	int i=0;
 	int j=0;
-	
-	*lg =0;
 
+	int *profondeur = 0;
+	
 	//on initialise le tableau
 	for(int k=0; k<32; k++){
 		res[k]=0;
 	}
 
 	//parcours de l'arbre en recherchant sym
-	n=recherche_symbole_arbre(tree,symbole);
+	n=recherche_symbole_arbre(tree,symbole, profondeur);
+
+	//le nombre de bit necessaire pour ecrire le symbole codé
+	(*lg)=(*profondeur);
+
+	//i indique dans quelle case du tableau on doit commencer à écrire
+	i=((*profondeur)-1)/8;
+	//on indique a partir de quel rang on doit ecrire les bits
+	j=(8-((*profondeur)%8);
 
 	//ecriture du code de sym
 	//tant qu'on ne pointe pas sur la tete, c'est à dire qu'on a fini de remonter l'arbre
 	while ( n != tree ){
+
 		//si la branche vaut 1 (fils droit)
-		if (n == n->parent->fils_droit)
-			res[i] +=  1 << j; //on ecrit 1 (bit) décallé en fonction de la profondeur 
+		if (n == n->parent->fils_droit){
+			res[i] +=  1 << j; //on ecrit 1 (bit) décallé en fonction de la profondeur
+		}
+
 		//si la branche vaut 0  (fils gauche) decalage inutile
 
 		//on incremente
 		n = n->parent;
-		(*lg)++;
+		//longueur du code du symbole restant à ecrire
 		j++;
+
 		//si on commence un nouvel octet
-		if((*lg) != 0 && (*lg)%8 == 0){
-			i++;
+		if(j == 8){
+			i--;
 			j=0;
 		}
 	}
+
+
 
 	return (char*)res;
 
@@ -67,10 +82,10 @@ char* encoder_symbole(tree* tree, char symbole, int* lg){
 /**
 * Parcours d'un arbre en recherchant un symbole (char ascii)
 * fonction recursive
-renvoit NULL si le symbole n'est pas trouvé
+* renvoit NULL si le symbole n'est pas trouvé
 * renvoit le noeud correspondant au symbole
 **/
-noeud* recherche_symbole_arbre(tree* tree, char symbole){
+noeud* recherche_symbole_arbre(tree* tree, char symbole, int* profondeur){
 	noeud* n;
 
 	//si on arrive en fin d'arbre
@@ -84,10 +99,14 @@ noeud* recherche_symbole_arbre(tree* tree, char symbole){
 	}
 	//sinon on parcours le fils droit pour le trouver
 	else if(n = recherche_symbole_arbre(tree->fils_droit, symbole) != NULL){
+		(*profondeur)++;
 		return n;
 	}
 	//et si il n'est pas dans le fils droit on parcours le fils gauche
-	else return recherche_symbole_arbre(tree->fils_gauche, symbole);
+	else {
+		(*profondeur)++;
+		return recherche_symbole_arbre(tree->fils_gauche, symbole);
+	}
 
 }
 
