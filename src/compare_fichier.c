2dfb26bae3bfec01aa54a690286c7de02751072a
raphael <raphael.audin@etud.univ-grenoble-alpes.fr>
2018-05-17 08:26:00 +0200

    optim du code

--- src/compare_fichier.c
+++ src/compare_fichier.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//#define min(a,b) ((a)<(b)?(a):(b))
 
 int lire_symbole(FILE* f){
     char c;
@@ -19,17 +18,12 @@ void compare_file(FILE* fichier ,FILE* fichier_bis)
 
     char lecture;
 
-	// prec=lire_symbole(fichier);
- //    courant=lire_symbole(fichier);
-
-
     if (fichier != NULL)
     {
 	    while(!(feof(fichier)))
 	    {	
 			lecture=lire_symbole(fichier);
 	    	compteur_f1++;	    
-	    	//printf("%c\n",lecture );	
 	    }
 	}
 	else printf("Probleme lecture fichier");
@@ -41,12 +35,10 @@ void compare_file(FILE* fichier ,FILE* fichier_bis)
 	    {	
 			lecture=lire_symbole(fichier_bis);
 	    	compteur_f2++;	    
-	    	//printf("%c\n",lecture );	
 	    }
 	}
 	else printf("Probleme lecture fichier");
 
-	//printf("%d",min(compteur_f1,compteur_f2));
 	if (compteur_f1<compteur_f2)
 	{
 		printf("le fichier: test ,est le plus petit\n");
@@ -62,6 +54,5 @@ int main()
 	FILE* fichier_bis = NULL;
     fichier = fopen("test", "r"); // ouverture en lecture
     fichier_bis = fopen("rle.txt", "r"); // ouverture en lecture
-	//printf("NB car test= %d\n",compare_file(fichier,fichier_bis));
     compare_file(fichier,fichier_bis);
 }
