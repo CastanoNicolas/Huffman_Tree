3cd362b3bbf370147ca9ee870cf535e99e0f52c4
Ariane Ancrenaz <ancrenaz.ariane@hotmail.fr>
2018-05-17 16:58:51 +0200

    ajout pretraitement

--- header/compression.h
+++ header/compression.h
@@ -1,4 +1,5 @@
 #ifndef COMPRESSION
 #define COMPRESSION
 void compression(char* file_source, char* file_destination);
+void compression_avec_pretaitement(char* file_source, char* file_destination);
 #endif
