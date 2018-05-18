508fe5ee7eb9bd7399e7e6f14ed995113b5995a8
QuSibue <quentin.sibue@live.fr>
2018-05-18 10:43:50 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -15,6 +15,7 @@ int main(int argc, char const *argv[]) {
   FILE* file_source;
   int c=0;
   int d=0;
+  int p=0;
   char* input = malloc(100*sizeof(char));
   char* output = malloc(100*sizeof(char));
   char* outputCompressed = malloc(strlen(input)+1);
@@ -22,7 +23,15 @@ int main(int argc, char const *argv[]) {
   int nbParam=0;
   int source=0;
   
-  int erreur = shell(&file_source,input,&source,argc,argv,&c,&d,&nbParam,output);
+  if(argc == 1){
+    printf("Exemple d'execution :\n");
+    printf("./argc0 -c fichier_entree -o fichier_compresse\n");
+    printf("./argc0 -d fichier_compresse -o fichier_decompresse\n");
+    printf("./argc0 -c fichier_entree -d -o fichier_decompresse\n");
+    printf("./argc0 -c fichier_entree -o fichier_compresse -p\n");
+    return -1;
+  }
+  int erreur = shell(&file_source,input,&source,argc,argv,&c,&d,&p,&nbParam,output);
   if(erreur != 0){
     affichage_erreur(erreur);
   }
@@ -43,25 +52,48 @@ int main(int argc, char const *argv[]) {
 
       if(d==1){
         sprintf(outputCompressed,"%s%s%c",input,".chf",'\0');
-        printf("On effectue la compression de %s vers %s\n",input,outputCompressed);
-        compression_avec_pretaitement(input,outputCompressed);
+        if(p==0){
+          printf("On effectue la compression de %s vers %s\n",input,outputCompressed);
+          compression(input,outputCompressed);
+        }
+        else{
+          printf("On effectue la compression avec pretraitement de %s vers %s\n",input,outputCompressed);
+          compression_avec_pretaitement(input,outputCompressed);
+        }
+        
       }
       else{
-        printf("On effectue la compression de %s vers %s\n",input,output);
-        compression_avec_pretaitement(input,output);
+        if(p==0){
+          printf("On effectue la compression de %s vers %s\n",input,output);
+          compression(input,output);
+        }
+        else{
+          printf("On effectue la compression avec pretraitement de %s vers %s\n",input,output);
+          compression_avec_pretaitement(input,output);
+        }  
       }
       
     }
     if (d == 1){
       if(c == 0){
-
-        printf("On effectue la decompression sur %s vers %s\n",input,output);
-        decompression_avec_pretraitement(input,output);
+        if(p==0){
+          printf("On effectue la decompression sur %s vers %s\n",input,output);
+          decompression(input,output);
+        }
+        else{
+          printf("On effectue la decompression avec pretraitement sur %s vers %s\n",input,output);
+          decompression_avec_pretraitement(input,output);
+        }
       }
       else{
-        
-        printf("On effectue la decompression sur %s vers %s\n",outputCompressed,output);
-        decompression_avec_pretraitement(outputCompressed,output);
+        if(p==0){
+          printf("On effectue la decompression sur %s vers %s\n",outputCompressed,output);
+          decompression(outputCompressed,output);
+        }
+        else{
+          printf("On effectue la decompression pretraitement sur %s vers %s\n",outputCompressed,output);
+          decompression_avec_pretraitement(outputCompressed,output);
+        }
       }
     }
     
